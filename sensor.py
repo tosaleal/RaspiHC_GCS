24a2c49da4855b62702ffdd664eba6d595e817c7
Tomas Salazar <tosaleal@gmail.com>
2022-07-19 11:42:43 -0500

    condition alert

--- sensor.py
+++ sensor.py
@@ -1,10 +1,13 @@
 from gpiozero import DistanceSensor
 from time import sleep
 
+threshold_distance=0.2
 sensor = DistanceSensor(echo=18, trigger=17,max_distance=2, threshold_distance=0.2)
 while True:
     d= sensor.distance *100 #distance in cm
     #condition to send alert
-    
-    print('Distance: ', sensor.distance * 100,sensor.when_out_of_range)
+    if (d<=threshold_distance):
+        print('Full bin')
+    else:
+        print('Distance: ', sensor.distance * 100,sensor.when_out_of_range)
     sleep(1)
