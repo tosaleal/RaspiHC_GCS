a0a06851103722177c5cb93b0d46016802fbdb82
Tomas Salazar <tosaleal@gmail.com>
2022-07-19 09:43:19 -0500

    error raspizero.. sensor distancesensor(3,2)

--- sensor.py
+++ sensor.py
@@ -1,11 +1,11 @@
-import gpiozero as gpio
+from gpiozero import DistanceSensor
 from time import sleep
 
 #pin definition
 trig = 2
 echo = 3
 
-sensor = gpio.DistanceSensor(echo,trig)
+sensor = DistanceSensor(3,2)
 
 while True:
     print('Distance: ',sensor.distance*100)
