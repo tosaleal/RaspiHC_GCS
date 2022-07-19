dadfdb649298d1d994f3a3fee0ba5ce89e431127
Tomas Salazar <tosaleal@gmail.com>
2022-07-19 11:33:30 -0500

    in range test

--- sensor.py
+++ sensor.py
@@ -3,5 +3,8 @@ from time import sleep
 
 sensor = DistanceSensor(echo=18, trigger=17,max_distance=2, threshold_distance=0.2)
 while True:
-    print('Distance: ', sensor.distance * 100)
+    d= sensor.distance *100 #distance in cm
+    #condition to send alert
+    
+    print('Distance: ', sensor.distance * 100,sensor.when_in_range)
     sleep(1)
