d2d0e1c407ac1f058a06d842b596b2f432bbe314
Tomas Salazar <tosaleal@gmail.com>
2022-07-19 10:36:06 -0500

    sensor works, added max and threshold distance

--- sensor.py
+++ sensor.py
@@ -1,7 +1,7 @@
 from gpiozero import DistanceSensor
 from time import sleep
 
-sensor = DistanceSensor(echo=18, trigger=17)
+sensor = DistanceSensor(echo=18, trigger=17,max_distance=2, threshold_distance=0.2)
 while True:
     print('Distance: ', sensor.distance * 100)
     sleep(1)
